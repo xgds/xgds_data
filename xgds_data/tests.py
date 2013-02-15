2f58257647d407f042eec77f011f36c2d2c8672e
Trey Smith <trey.smith@nasa.gov>
2013-02-15 13:00:04 -0800

    created xgds_data app from template

---
+++ xgds_data/tests.py
@@ -0,0 +1,15 @@
+# __BEGIN_LICENSE__
+# Copyright (C) 2008-2010 United States Government as represented by
+# the Administrator of the National Aeronautics and Space Administration.
+# All Rights Reserved.
+# __END_LICENSE__
+
+from django.test import TestCase
+
+
+class xgds_dataTest(TestCase):
+    """
+    Tests for xgds_data
+    """
+    def test_xgds_data(self):
+        pass
