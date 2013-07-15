f187e789a091c57081a41c9e0dac7dee08b9cf21
Shawn Wolfe <Shawn.R.Wolfe@nasa.gov>
2013-07-15 11:11:23 -0700

    Restoring code that was accidentally munched.

--- xgds_data/views.py
+++ xgds_data/views.py
@@ -39,8 +39,104 @@ def index(request):
 
 # from django import forms ## need to get this out of here and back into form
 
+def getModelInfo(qualifiedName, model):
+    return {
+        'name': model.__name__,
+        'qualifiedName': qualifiedName
+    }
+
+SEARCH_MODELS = dict([(name, getModelByName(name))
+                      for name in settings.XGDS_DATA_SEARCH_MODELS])
+
+MODELS_INFO = [getModelInfo(qualifiedName, model)
+              for qualifiedName, model in SEARCH_MODELS.iteritems()]
+
+
+def searchIndex(request):
+    return render_to_response('xgds_data/searchIndex.html',
+                              {'models': MODELS_INFO},
+                              context_instance=RequestContext(request))
+
+
+def searchModel(request, modelName):
+    if request.method not in ('GET', 'POST'):
+        return HttpResponseNotAllowed(['GET', 'POST'])
+
+    model = SEARCH_MODELS[modelName]
+    tableName = model._meta.db_table
+    modelInfo = getModelInfo(modelName, model)
+
+    fieldLookup = dict(((field.name, field)
+                        for field in model._meta._fields()))
+    timestampField = None
+    for field in ('timestampSeconds', 'timestamp'):
+        if field in fieldLookup:
+            timestampField = field
+            break
+
+    if request.method == 'POST':
+        form = QueryForm(request.POST)
+        assert form.is_valid()
+        userQuery = form.cleaned_data['query']
+        escapedUserQuery = re.sub(r'%', '%%', userQuery)
+        mostRecentFirst = form.cleaned_data['mostRecentFirst']
+
+        prefix = 'SELECT * FROM %s ' % tableName
+        countPrefix = 'SELECT COUNT(*) FROM %s ' % tableName
+        order = ''
+        if mostRecentFirst and timestampField:
+            order += ' ORDER BY %s DESC ' % timestampField
+        limit = ' LIMIT 100'
+
+        countQuery = countPrefix + escapedUserQuery
+        sqlQuery = prefix + escapedUserQuery + order + limit
+        # escape % signs, interpreted by Django raw() as template format
+
+        styledSql = (prefix
+                     + '<span style="color: blue; font-weight: bold">%s</span>' % userQuery
+                     + order
+                     + limit)
+
+
+        try:
+            cursor = connection.cursor()
+            cursor.execute(countQuery)
+            count = cursor.fetchone()[0]
+
+            matches = list(model.objects.raw(sqlQuery))
+
+            wasError = False
+        except DatabaseError:
+            sys.stderr.write(traceback.format_exc())
+            wasError = True
+            result = {
+                'sql': styledSql,
+                'summary': 'database error!',
+                'matches': [],
+            }
+        if not wasError:
+            result = {
+                'sql': styledSql,
+                'summary': '%s matches (showing at most 100)' % count,
+                'matches': matches,
+            }
+    else:
+        # GET method
+        form = QueryForm()
+        result = None
+    return render_to_response('xgds_data/searchModel.html',
+                              {'model': modelInfo,
+                               'models': MODELS_INFO,
+                               'form': form,
+                               'result': result},
+                              context_instance=RequestContext(request))
+
+
+SKIP_APP_REGEXES = [re.compile(p) for p in settings.XGDS_DATA_SEARCH_SKIP_APP_PATTERNS]
+
+
 def isSkippedApp(appName):
-    return (appName.find('django') > -1)
+    return any((r.match(appName) for r in SKIP_APP_REGEXES))
 
 
 def hasModels(appName):
