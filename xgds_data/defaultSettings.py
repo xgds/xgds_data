dbf57168d831957b99237f2f7a75c2c488fd9b5a
Tamar Cohen <tamar.e.cohen@nasa.gov>
2015-02-18 16:48:27 -0800

    changes for bower

--- xgds_data/defaultSettings.py
+++ xgds_data/defaultSettings.py
@@ -47,3 +47,8 @@ XGDS_DATA_TIME_FIELDS = (
     'timeStampSeconds',
     'timestamp',
 )
+
+# include this in your siteSettings.py BOWER_INSTALLED_APPS
+XGDS_DATA_BOWER_INSTALLED_APPS = ('flot',
+                                  'datatables-fixedcolumns'
+                                  )
